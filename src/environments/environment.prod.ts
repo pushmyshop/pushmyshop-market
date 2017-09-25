c58615236e5d46e2c38f2b18e5f210171051d24c
Yan MATAGNE <ymatagne@norsys.fr>
2017-09-25 13:00:07 -0400

    fix(prod) : change domaine name

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  urlOfApi: 'http://pushmyshop-api.com/',
+  urlOfApi: 'http://api.pushmyshop.com/',
   vapidPublicKey: 'BGgM0ydbnR-04QSrQyPMC0JMqA7xF6TSQ8Wb7rtKa5PSTdOjpSmdydadrrlwuPziFhUM8DodMt_t09nDi7oFtSY'
 };
