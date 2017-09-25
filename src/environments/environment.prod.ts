e806d9612ab06afc3f19e0cfb48b6f396acc15be
Yan MATAGNE <ymatagne@norsys.fr>
2017-09-24 20:13:26 -0400

    feat(docker): init Dockerfile and docker process

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  urlOfApi: 'http://localhost:8080',
+  urlOfApi: 'http://pushmyshop-api.com/',
   vapidPublicKey: 'BGgM0ydbnR-04QSrQyPMC0JMqA7xF6TSQ8Wb7rtKa5PSTdOjpSmdydadrrlwuPziFhUM8DodMt_t09nDi7oFtSY'
 };
