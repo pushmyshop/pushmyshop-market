2009d2efcd1ea7fb92d02366d67264028e05a556
Yan MATAGNE <ymatagne@norsys.fr>
2017-09-27 09:02:21 -0400

    fix(prod) : add log for second process

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  urlOfApi: 'https://api.pushmyshop.com/',
+  urlOfApi: 'https://api.pushmyshop.com',
   vapidPublicKey: 'BGgM0ydbnR-04QSrQyPMC0JMqA7xF6TSQ8Wb7rtKa5PSTdOjpSmdydadrrlwuPziFhUM8DodMt_t09nDi7oFtSY'
 };
